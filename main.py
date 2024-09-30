b0ccc8242b5688e6ea0c1022dda6e45407a8d624
Brier Hehmeyer <bmhehmeyer@gmail.com>
2024-09-29 22:37:15 -0700

    Background pic and basic stuff

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame
 import sys
 
 
+def scale_background_image(image, screen_size):
+    image_size = image.get_size()
+    scene_scaler = min(screen_size[0] / image_size[0], screen_size[1] / image_size[1])
+    scaled_image = pygame.transform.scale(image, (image_size[0] * scene_scaler, image_size[1] * scene_scaler))
+    scaled_image_size = scaled_image.get_size()
+
+    background_surface = pygame.Surface(screen_size)
+    background_surface.fill((0, 0, 0))
+    background_surface.blit(scaled_image, ((screen_size[0] - scaled_image_size[0]) / 2, (screen_size[1] - scaled_image_size[1]) / 2))
+    return background_surface
+
+
 def main():
     pygame.init()
     pygame.event.set_allowed([pygame.KEYDOWN, pygame.QUIT, pygame.KEYUP, pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP])
@@ -25,10 +37,21 @@ def main():
 
     font = pygame.font.SysFont("arial", 36)
 
+    bed_scene_image = scale_background_image(pygame.image.load("./Images/Scenes/Bed Scene.webp"), screen_size)
+
+    current_background = bed_scene_image
+    effective_screen_size = current_background.get_size()
+
+    text_box = pygame.image.load("./Images/Text Box.png")
+    text_box = pygame.transform.scale(text_box, (current_background.get_width() - 500,
+                                                 text_box.get_height() * ((current_background.get_width() - 500) / text_box.get_width())))
+    text_box_size = text_box.get_size()
+
     while True:
-        screen.fill(black)
+        screen.blit(bed_scene_image, (0, 0))
         delta_time = clock.tick(fps) / 1000
         screen.blit(font.render("Hello, World!", True, white), (0, 0))
+        screen.blit(text_box, ((screen_width - text_box_size[0]) / 2, effective_screen_size[1] - text_box_size[1] - 30))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ text.py
@@ -0,0 +1,8 @@
+import pygame
+
+
+font = pygame.font.SysFont("arial", 18)
+
+
+class Text:
+    def __init__(self, text, text_box):
